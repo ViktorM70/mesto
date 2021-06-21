6798d37b32907e24ee458bde53bc25900b8270ce
Viktor Makeev <vikt0rmakeev@yandex.ru>
2021-06-22 01:15:37 +0700

    priject_4C

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,25 @@
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
-let popupClose = popup.querySelector('.popup__close-button');
+let closeButton = popup.querySelector('.popup__close-button');
 let profileName = document.querySelector('.profile__name');
 let profileProfession = document.querySelector('.profile__profession');
 let popupContainer = popup.querySelector('.popup__container'); 
-let popupName = popup.querySelector('.popup__text_name'); 
-let popupProfession = popup.querySelector('.popup__text_profession');
+let popupName = popup.querySelector('.popup__text_type_name'); 
+let popupProfession = popup.querySelector('.popup__text_type_profession');
 
-editButton.addEventListener('click', function() {
-  popup.classList.add('popup_opened');
+function openPopup () {
   popupName.value = profileName.textContent;
   popupProfession.value = profileProfession.textContent;
-})
+  popup.classList.add('popup_opened');
+}
 
-popupClose.addEventListener('click', function() {
+editButton.addEventListener('click', openPopup);
+
+function closePopup () {
   popup.classList.remove('popup_opened');
-})
+}
+
+closeButton.addEventListener('click', closePopup);
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
